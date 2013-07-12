a5a49a0120ad7463029983b3824fc8ddbb75c728
stackptr <corey@x64.co>
2013-07-12 15:37:52 -0400

    test input processing for date strings

--- tests/input.pl
+++ tests/input.pl
@@ -4,6 +4,8 @@ use warnings;
 use utf8;
 use Data::Dumper;
 use Text::Balanced ("extract_delimited");
+BEGIN{push @INC, "../lib"; }
+use DateTime::Format::Flexible;
 
 open(my $input, "<", "test_input") or die $!;
 open(my $multi, "<", "test_multi") or die $!;
@@ -25,14 +27,43 @@ while(<$multi>){
     $_ =~ s/"//g foreach @fields;
     
     my $keyword = shift @fields;
-    my @aliases = \@fields;
+    my (@aliases, @dates);
+
+    # Determine date strings or alias strings
+    foreach (@fields){
+        my $date_regex = "^(19|20)\\d\\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])\$";
+        if (/$date_regex/){
+            if ($#dates < 1 ){
+                push @dates, parse($_);
+            } else {
+                next;
+            }
+
+        } else {
+            push @aliases, $_;
+        }
+    }
+
+    # Process dates
+    my ($begin, $end);
+    @dates = sort @dates;
+    $begin = $dates[0];
+    $end = $dates[1];
     
     my %keyword_data = (
         aliases => \@aliases,
         pages => [0, 0, 0],
+        begin => $begin,
+        end => $end
     );
     $keywords{$keyword} = \%keyword_data;
 }
 
 print Dumper (\%keywords);
 
+
+sub parse {
+    my $str = shift;
+    my $ret = DateTime::Format::Flexible->parse_datetime($str);
+    return $ret->epoch();
+}
